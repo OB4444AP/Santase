8ba1e4aa500197e952598149f3abccabc31c5f96
OB4444AP <mitkodragiev68@gmail.com>
2026-01-22 12:06:40 +0200

    Added savePlayer and loadPlayer functions

--- Santase/saveLoad.cpp
+++ Santase/saveLoad.cpp
@@ -11,3 +11,40 @@ void saveCard(std::ofstream& file, const Card& c) {
 void loadCard(std::ifstream& file, Card& c) {
     file >> c.suit >> c.value;
 }
+
+void savePlayer(std::ofstream& file, const Player& p) {
+    file << p.name << " " << p.trickPoints << " " << p.gamePoints << " "
+        << p.hasSurrendered << " " << p.hasSurrenderedForever << " " << p.hasEnded << " "
+        << p.tricksWon << " " << p.roundsWon << "\n";
+
+    saveCard(file, p.cardPlayed);
+    saveCard(file, p.lastTrickCard);
+
+    file << p.handSize << "\n";
+    for (int i = 0; i < p.handSize; i++) {
+        saveCard(file, p.hand[i]);
+    }
+}
+
+void loadPlayer(std::ifstream& file, Player& p) {
+    file >> p.name >> p.trickPoints >> p.gamePoints
+        >> p.hasSurrendered >> p.hasSurrenderedForever >> p.hasEnded
+        >> p.tricksWon >> p.roundsWon;
+
+    loadCard(file, p.cardPlayed);
+    loadCard(file, p.lastTrickCard);
+
+    int loadedHandSize;
+    file >> loadedHandSize;
+
+    if (p.hand != nullptr) {
+        delete[] p.hand;
+    }
+
+    p.hand = new Card[MAX_HAND_SIZE];
+    p.handSize = loadedHandSize;
+
+    for (int i = 0; i < p.handSize; i++) {
+        loadCard(file, p.hand[i]);
+    }
+}
--- Santase/saveLoad.h
+++ Santase/saveLoad.h
@@ -2,3 +2,5 @@
 
 void saveCard(std::ofstream& file, const Card& c);
 void loadCard(std::ifstream& file, Card& c);
+void savePlayer(std::ofstream& file, const Player& p);
+void loadPlayer(std::ifstream& file, Player& p);
